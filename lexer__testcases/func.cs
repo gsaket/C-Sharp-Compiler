3bf6ebb4265fdceb873ca32ba2ecdce68319f130
gsaket <gsaket@iitk.ac.in>
2018-02-03 15:57:57 +0530

    Converted C codes to C#

---
+++ lexer__testcases/func.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class test5{
+     public static void Main() {
+          foo();
+          Console.ReadKey();
+     }
+     void foo(){
+          int i=1;
+     }
+}
---
+++ lexer__testcases/struct_func.cs
@@ -0,0 +1,31 @@
+using System;
+
+typedef struct
+{
+  char a;
+  int b;
+  char c;
+  short d;
+  double e;
+  char name[10];
+  char f;
+} T;
+
+
+public class test8{
+     public static void Main(){
+          T k;
+          f(k);
+          Console.ReadKey();
+     }
+     void f (T x)
+     {
+          x.a = 'a';
+          x.b = 47114711;
+          x.c = 'c';
+          x.d = 1234;
+          x.e = 3.141592897932;
+          x.f = '*';
+          x.name = "abc";
+     }
+}
